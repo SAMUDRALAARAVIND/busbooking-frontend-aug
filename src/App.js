da155832d714dbd4bc362af2fbe38fff6f3efb52
manishloginn <manishv7714@gmail.com>
2024-09-15 19:36:25 +0530

    Save changes before switching branches

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-const Search = lazy(() => import("./feature/search/index"));
-const TripsScreen = lazy(() => import("./feature/trips/index"));
-const BookingScreen = lazy(() => import("./feature/booking/index"));
+// import { lazy, Suspense } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Mock from "./Mock";
+// const Search = lazy(() => import("./feature/search/index"));
+// const TripsScreen = lazy(() => import("./feature/trips/index"));
+// const BookingScreen = lazy(() => import("./feature/booking/index"));
 
-const LazyLoadingWrapper = ({ Component }) => {
-  return <Suspense fallback={<h1>Loading ...</h1>}>{<Component />}</Suspense>;
-};
+// const LazyLoadingWrapper = ({ Component }) => {
+//   return <Suspense fallback={<h1>Loading ...</h1>}>{<Component />}</Suspense>;
+// };
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<LazyLoadingWrapper Component={Search} />} />
-        <Route
-          path="/trips/search/:source/:sourceId/:destionation/:destinationId"
-          element={<LazyLoadingWrapper Component={TripsScreen} />}
-        />
-        <Route
-          path="/book"
-          element={<LazyLoadingWrapper Component={BookingScreen} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="" element={<LazyLoadingWrapper Component={Search} />} />
+    //     <Route
+    //       path="/trips/search/:source/:sourceId/:destionation/:destinationId"
+    //       element={<LazyLoadingWrapper Component={TripsScreen} />}
+    //     />
+    //     <Route
+    //       path="/book"
+    //       element={<LazyLoadingWrapper Component={BookingScreen} />}
+    //     />
+    //   </Routes>
+    // </BrowserRouter>
+
+    <Mock />
   );
 };
 
