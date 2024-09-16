ec7e2631cdc7ccff3f825419d19748ec8e592671
manishloginn <manishv7714@gmail.com>
2024-09-16 22:58:45 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Mock from "./Mock";
 const Search = lazy(() => import("./feature/search/index"));
 const TripsScreen = lazy(() => import("./feature/trips/index"));
 const BookingScreen = lazy(() => import("./feature/booking/index"));
