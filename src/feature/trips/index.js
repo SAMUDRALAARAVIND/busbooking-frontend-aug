a5272857a88de4193bc4b853ea052d4402eab113
manishloginn <manishv7714@gmail.com>
2024-09-18 17:21:30 +0530

    changes

--- src/feature/trips/index.js
+++ src/feature/trips/index.js
@@ -2,11 +2,13 @@ import Filters from "../filters";
 import Navbar from "../search/Navbar";
 import TripsCard from "./components/tripsCard";
 
+import "./styles/main.scss"
+
 const TripsScreen = () => {
   return (
     <>
       <Navbar />
-      <div className="trips-screen flex">
+      <div className="trips-screen">
         <Filters />
         <TripsCard />
       </div>
