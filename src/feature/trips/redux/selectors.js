a91074539b3ddf3407f530bb5041dca139197b1c
kajal Gupta <kajalg1401@gmail..com>
2024-09-27 08:55:19 +0530

    added createSelectore

--- src/feature/trips/redux/selectors.js
+++ src/feature/trips/redux/selectors.js
@@ -6,10 +6,8 @@ import { createSelector } from "reselect";
 
 
 export const tripsStatusSelector = (state) => state.trips.apiStatus;
-
 const tripsResponseSelector = (state) => state?.trips?.tripsResponse;
 const filtersSelector = (state) => state?.filters;
-
 
 export const tripsSelector = createSelector(
   [tripsResponseSelector, filtersSelector],
@@ -18,93 +16,90 @@ export const tripsSelector = createSelector(
   const mainBoardingPoints = tripsResponseSelector?.boardingPoints || [];
   const mainDroppingPoints = tripsResponseSelector?.dropingPoints || [];
 
+  console.log("filtersSelector", filtersSelector)
   // filtering Trips Data
   const filteredTrips = tripsResponseSelector.trips
     .filter((trip) => {
-      console.log("trip", trip)
       const busTypes = Object.keys(filtersSelector[filterType.BUS_TYPES]).filter(
         (key) => filtersSelector[filterType.BUS_TYPES][key]
       );
-console.log("busTypes", busTypes)
-console.log("trips_bustypoes", busTypes.includes(trip.busType))
-
       if (busTypes.length === 0) return true;
       return busTypes.includes(trip.busType);
     })
+    // .filter((trip) => {
+    //   const [min, max] = filtersSelector[filterType.PRICE_RANGE].selectedRange || [
+    //     0,
+    //     Infinity,
+    //   ];
+    //   return trip.maxPrice >= min && trip.minPrice <= max;
+    // })
     .filter((trip) => {
-      const [min, max] = filtersSelector[filterType.PRICE_RANGE].selectedRange || [
-        0,
-        Infinity,
-      ];
-      return trip.maxPrice >= min && trip.minPrice <= max;
+      const selectedPartners = busPartners.filter(
+        (partner) => filtersSelector[filterType.BUS_PARTNER][partner]
+      );
+      if (selectedPartners.length === 0) return true;
+      return selectedPartners.includes(trip.busPartner);
     })
-  //   .filter((trip) => {
-  //     const selectedPartners = busPartners.filter(
-  //       (partner) => filtersSelector[filterType.BUS_PARTNER][partner]
-  //     );
-  //     if (selectedPartners.length === 0) return true;
-  //     return selectedPartners.includes(trip.busPartner);
-  //   })
-  //   .filter((trip) => {
-  //     const selectedBoardingPoints = filtersSelector[filterType.BOARDING_POINTS];
-  //     if (Object.keys(selectedBoardingPoints).length === 0) return true;
-  //     // const mainBoardingPoints = state?.trips?.tripsResponse.boardingPoints;
+    .filter((trip) => {
+      const selectedBoardingPoints = filtersSelector[filterType.BOARDING_POINTS];
+      if (Object.keys(selectedBoardingPoints).length === 0) return true;
+      // const mainBoardingPoints = state?.trips?.tripsResponse.boardingPoints;
 
-  //     const sourceStops = trip?.boardingPoints ?? [];
-  //     return sourceStops.some((stop) => {
-  //       const mainStop = mainBoardingPoints.find(
-  //         (point) => point.stopId === stop.stopId
-  //       );
+      const sourceStops = trip?.boardingPoints ?? [];
+      return sourceStops.some((stop) => {
+        const mainStop = mainBoardingPoints.find(
+          (point) => point.stopId === stop.stopId
+        );
 
-  //       return mainStop && selectedBoardingPoints[mainStop.stopId];
-  //     });
-  //   })
-  //   .filter((trip) => {
-  //     const selectedDroppingPoints = filtersSelector[filterType.DROPPING_POINTS];
-  //     if (Object.keys(selectedDroppingPoints).length === 0) return true;
+        return mainStop && selectedBoardingPoints[mainStop.stopId];
+      });
+    })
+    .filter((trip) => {
+      const selectedDroppingPoints = filtersSelector[filterType.DROPPING_POINTS];
+      if (Object.keys(selectedDroppingPoints).length === 0) return true;
 
-  // // drropping points
-  //     const destinationStops = trip?.droppingPoints ?? [];
-  //     return destinationStops.some((stop) => {
-  //       const mainStop = mainDroppingPoints.find(
-  //         (point) => point.stopId === stop.stopId
-  //       );
-  //       return mainStop && selectedDroppingPoints[mainStop.stopId];
-  //     });
-  //   })
-  //   .filter((trip) => {
-  //     const selectedDepartureTimes = Object.keys(
-  //       filtersSelector[filterType.DEPARTURE_TIME]  ).filter((key) => filtersSelector[filterType.DEPARTURE_TIME][key]);
-  //     if (selectedDepartureTimes.length === 0) return true;
+  // drropping points
+      const destinationStops = trip?.droppingPoints ?? [];
+      return destinationStops.some((stop) => {
+        const mainStop = mainDroppingPoints.find(
+          (point) => point.stopId === stop.stopId
+        );
+        return mainStop && selectedDroppingPoints[mainStop.stopId];
+      });
+    })
+    .filter((trip) => {
+      console.log("tripsss", trip)
+      const selectedDepartureTimes = Object.keys(
+        filtersSelector[filterType.DEPARTURE_TIME]  ).filter((key) => filtersSelector[filterType.DEPARTURE_TIME][key]);
+      if (selectedDepartureTimes.length === 0) return true;
 
-  //     const date = new Date(window.location.href.split("/").slice(-1)[0]);
-  //     console.log("date", date )
-  //     const time10AM = new Date(date).setHours(10, 0, 0, 0);
-  //     const time5PM = new Date(date).setHours(17, 0, 0, 0);
-  //     const time11PM = new Date(date).setHours(23, 0, 0, 0);
+      const date = new Date(window.location.href.split("/").slice(-1)[0]);
+      const time10AM = new Date(date).setHours(10, 0, 0, 0);
+      const time5PM = new Date(date).setHours(17, 0, 0, 0);
+      const time11PM = new Date(date).setHours(23, 0, 0, 0);
 
-  //     let filtered = false;
-  //     trip?.boardingPoints?.forEach((stop) => {
-  //       const arrivalTime = stop.arrivalTime;
-  //       if (selectedDepartureTimes.includes(departureTime.MORNING)) {
-  //         filtered ||= arrivalTime <= time10AM;
-  //       }
-  //       if (selectedDepartureTimes.includes(departureTime.AFTERNOON)) {
-  //         filtered ||= arrivalTime >= time10AM && arrivalTime <= time5PM;
-  //       }
-  //       if (selectedDepartureTimes.includes(departureTime.EVENING)) {
-  //         filtered ||= arrivalTime >= time5PM && arrivalTime <= time11PM;
-  //       }
-  //       if (selectedDepartureTimes.includes(departureTime.NIGHT)) {
-  //         filtered ||= arrivalTime >= time11PM;
-  //       }
-  //     });
-
-  //     return filtered;
-  //   });
-
+      let filtered = false;
+      trip?.boardingPoints?.forEach((stop) => {
+        const arrivalTime = stop.arrivalTime;
+        console.log("dpartre",departureTime.MORNING)
+        console.log("dpp",arrivalTime)
+        if (selectedDepartureTimes.includes(departureTime.MORNING)) {
+          filtered ||= arrivalTime <= time10AM;
+        }
+        if (selectedDepartureTimes.includes(departureTime.AFTERNOON)) {
+          filtered ||= arrivalTime >= time10AM && arrivalTime <= time5PM;
+        }
+        if (selectedDepartureTimes.includes(departureTime.EVENING)) {
+          filtered ||= arrivalTime >= time5PM && arrivalTime <= time11PM;
+        }
+        if (selectedDepartureTimes.includes(departureTime.NIGHT)) {
+          filtered ||= arrivalTime >= time11PM;
+        }
+      });
+console.log("filtered", filtered)
+      return filtered;
+    });
     console.log("tripsResponse", filteredTrips)
-
     return {
       filteredTrips: filteredTrips.length > 0 ?  filteredTrips : tripsResponseSelector.trips,
       mainBoardingPoints,
